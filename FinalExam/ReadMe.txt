9a17f67b46a0c5ac38005671f1c090c91983a05b
Hsung1536 <127166787+Hsung1536@users.noreply.github.com>
2024-06-14 11:58:55 +0900

    FinalExam

---
+++ FinalExam/ReadMe.txt
@@ -0,0 +1,34 @@
+
+GITHUB
+
+1. GLFW 라이브러리를 이용해서 window를 생성하시오.
+- Window의 이름은 "Google Dino Run Copy Game" 으로 해주시고 800X600 해상도로 해주세요.
+- 화면의 1px은 기획서 상의 1cm와 같습니다.
+
+
+2. 
+
+
+3. Transform 함수들을 작성하시오. (미터단위 이동, 각도단위 회전, 미터단위 배율조정)
+- 단 회전은 시계 방향으로 처리하시오.
+
+int MoveVertex(Vertex point, Vector meter)
+{
+	return 0;
+}
+
+int RotateVertex(Vertex point, float angle_degree) 
+{
+	return 0;
+}
+
+int ScaleVertex(Vertex point, Vector meter)
+{
+	return 0;
+}
+
+4. 플레이어 캐릭터는 
+
+
+5. 장애물은 재활용할것.
+
---
+++ FinalExam/main.cpp
@@ -0,0 +1,103 @@
+﻿#pragma comment(lib, "Opengl32.lib")
+
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <cmath>
+
+float moveFactor = 0.0f;
+float scaleFactor = 1.0f;
+
+void errorCallback(int error, const char* description)
+{
+	std::cerr << "GLFW Error: " << description << std::endl;
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+
+}
+
+struct Vertex
+{
+	float x;
+	float y;
+};
+
+struct Vector
+{
+	float x_meter;
+	float y_meter;
+};
+
+int MoveVertex(Vertex point, Vector meter)
+{
+	return 0;
+}
+
+int RotateVertex(Vertex point, float angle_degree)
+{
+	return 0;
+}
+
+int ScaleVertex(Vertex point, Vector meter)
+{
+	return 0;
+}
+
+
+float angle = 0;
+
+int Initialize()
+{
+	return 0;
+}
+
+int Update()
+{
+	return 0;
+}
+
+int Render()
+{
+	return 0;
+}
+
+
+int main(void)
+{
+	//glfw라이브러리 초기화
+	if (!glfwInit())
+		return -1;
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(800, 800, "MuSoeunEngine", NULL, NULL);
+
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetErrorCallback(errorCallback);
+	glfwSetKeyCallback(window, keyCallback);
+
+	Initialize();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		Update();
+		Render();
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+
+}
